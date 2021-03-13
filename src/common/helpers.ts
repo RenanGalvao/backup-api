8132af6e1b20c5c59d1b8c7af6b5425790389438
Renan Gãlvão <renan.m.galvao@gmail.com>
2021-03-13 13:45:31 -0300

    fix: forcing corret date:hour format

--- src/common/helpers.ts
+++ src/common/helpers.ts
@@ -3,7 +3,7 @@ import { Logger } from '@nestjs/common';
 const logger = new Logger('Helpers');
 
 export function fileNameTimestamp(database: string){
-    return `${new Date().toLocaleString().replace(/[\/\s:]/g, '-')}-${database}.sql`;
+    return `${new Date().toLocaleString('pt-BR').replace(/[\/\s:]/g, '-')}-${database}.sql`;
 }
 
 export async function removeBadDump(database: string){
